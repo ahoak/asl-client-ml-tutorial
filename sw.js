825b039944aca3ec751ebbffafbec8bc705f7c34
stopyoukid <stopyoukid@users.noreply.github.com>
2022-08-23 22:16:21 +0000

    deploy: cd6795f0589c0b3686668e55c1cd52479bf773da

---
+++ sw.js
@@ -0,0 +1,36 @@
+/*
+ * ServiceWorker to make site function as a PWA (Progressive Web App)
+ *
+ * Based on https://glitch.com/~pwa by https://glitch.com/@PaulKinlan
+ */
+
+// Specify what we want added to the cache for offline use
+// self.addEventListener("install", e => {
+
+//   e.waitUntil(
+//     // Give the cache a name
+//     caches.open("tutorial").then(cache => {
+//       // Cache the homepage and stylesheets - add any assets you want to cache!
+//       return cache.addAll([
+//         "/", 
+//         "/styles/style.css",
+//         "/styles/themes/glitch.css",
+//         "/styles/themes/gallery.css",
+//         "/styles/themes/menu.css",
+//         "/styles/themes/custom-theme.css"
+//       ]);
+//     })
+//   );
+// });
+
+// Network falling back to cache approach
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+
+    fetch(event.request).catch(function() {
+
+      return caches.match(event.request);
+    })
+  );
+});
+// WebAssembly.instantiate(fetch("/tfjs-backend-wasm.wasm"))

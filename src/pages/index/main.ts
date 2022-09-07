575531c89bba33e6b825df710846f178c5deb669
Amber Hoak <amhoak@microsoft.com>
2022-09-07 09:03:45 -0700

    fix: build error

--- src/pages/index/main.ts
+++ src/pages/index/main.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import '../../utils/fluentBootstrap';
 
-import { getOrCreateElement, styleNavOnChange } from '../../utils/utils';
+import { styleNavOnChange } from '../../utils/utils';
 // console.log(`home page js loaded`);
 styleNavOnChange('home');

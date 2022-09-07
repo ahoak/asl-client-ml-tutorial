d2e619721a668e8d34bf205400cee0ff074a6aef
Amber Hoak <amhoak@microsoft.com>
2022-09-06 18:40:11 -0700

    feat: add content to home page

--- src/pages/index/main.ts
+++ src/pages/index/main.ts
@@ -1,4 +1,6 @@
-import { styleNavOnChange } from '../../utils/utils';
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import '../../utils/fluentBootstrap';
 
+import { getOrCreateElement, styleNavOnChange } from '../../utils/utils';
 // console.log(`home page js loaded`);
 styleNavOnChange('home');
